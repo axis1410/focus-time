ff52ac357e30b7bc1b972e8bb80350d63e62f5d3
Axis <62322233+axis1410@users.noreply.github.com>
2022-10-24 11:34:57 +0530

    Optimizations

--- App.js
+++ App.js
@@ -1,45 +1,37 @@
-import React, { useState } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from 'react-native';
-import Constants from 'expo-constants';
-import { colors } from './src/utils/colors';
-import { Focus } from './src/features/focus';
-import { Timer } from './src/features/timer';
-import { FocusHistory } from './src/features/FocusHistory';
+import React, { useState } from "react";
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
+import { colors } from "./src/utils/colors";
+import { Focus } from "./src/features/focus";
+import { Timer } from "./src/features/timer";
+import { FocusHistory } from "./src/features/FocusHistory";
 
 export default function App() {
-  const [currentSubject, setCurrentSubject] = useState();
-  const [history, setHistory] = useState([]);
+   const [currentSubject, setCurrentSubject] = useState();
+   const [history, setHistory] = useState([]);
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {!currentSubject ? (
-        <>
-          <Focus addSubject={setCurrentSubject} />
-          <FocusHistory history={history} />
-        </>
-      ) : (
-        <Timer
-          focusSubject={currentSubject}
-          onTimerEnd={(subject) => {
-            setHistory([...history, subject]);
-          }}
-          clearSubject={() => setCurrentSubject(null)}
-        />
-      )}
-    </SafeAreaView>
-  );
+   return (
+      <SafeAreaView style={styles.container}>
+         {!currentSubject ? (
+            <>
+               <Focus addSubject={setCurrentSubject} />
+               <FocusHistory history={history} />
+            </>
+         ) : (
+            <Timer
+               focusSubject={currentSubject}
+               onTimerEnd={(subject) => {
+                  setHistory([...history, subject]);
+               }}
+               clearSubject={() => setCurrentSubject(null)}
+            />
+         )}
+      </SafeAreaView>
+   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    backgroundColor: colors.backgroundGray,
-  },
+   container: {
+      flex: 1,
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      backgroundColor: colors.backgroundGray,
+   },
 });
